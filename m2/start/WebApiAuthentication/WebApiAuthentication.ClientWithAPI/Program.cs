4443aa9a4bc91b4828ad02e91e2e54ff63549153
Kevin Dockx <kevin.dockx@gmail.com>
2024-12-04 09:57:30 +0100

    wip

--- m2/start/WebApiAuthentication/WebApiAuthentication.ClientWithAPI/Program.cs
+++ m2/start/WebApiAuthentication/WebApiAuthentication.ClientWithAPI/Program.cs
@@ -18,11 +18,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets(); 
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
