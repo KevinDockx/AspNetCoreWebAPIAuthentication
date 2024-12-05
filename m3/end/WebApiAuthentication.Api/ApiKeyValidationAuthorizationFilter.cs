6bbc882e91039c51b271b22d6d2c5c3501e09c29
Kevin Dockx <kevin.dockx@gmail.com>
2024-12-05 11:05:36 +0100

    wip

---
+++ m3/end/WebApiAuthentication.Api/ApiKeyValidationAuthorizationFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiAuthentication.Api;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public class ApiKeyValidationAuthorizationFilter : Attribute, IAuthorizationFilter
+{ 
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!IsValidApiKey(context))
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+
+    private bool IsValidApiKey(AuthorizationFilterContext context)
+    {
+        // check if we've got the required input 
+        var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+        var validApiKey = config.GetValue<string>("Authentication:ApiKey");
+
+        if (string.IsNullOrWhiteSpace(validApiKey))
+        {
+            throw new KeyNotFoundException("ApiKey not found in configuration.");
+        }
+
+        // try and get the key from a custom request header "X-ApiKey"
+        if (!context.HttpContext.Request.Headers.TryGetValue("X-ApiKey", out var extractedApiKey))
+        {
+            // as a fallback option, try getting it from the query string           
+            if (!context.HttpContext.Request.Query.TryGetValue("X-ApiKey", out extractedApiKey))
+            {
+                return false;
+            }
+        }
+
+        return validApiKey.Equals(extractedApiKey); 
+    }
+}
---
+++ m3/end/WebApiAuthentication.Api/ApiKeyValidationMiddleware.cs
@@ -0,0 +1,43 @@
+﻿namespace WebApiAuthentication.Api;
+
+public class ApiKeyValidationMiddleware
+{
+    private readonly RequestDelegate _next; 
+
+    public ApiKeyValidationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var config = context.RequestServices.GetRequiredService<IConfiguration>();
+        var validApiKey = config.GetValue<string>("Authentication:ApiKey");
+
+        if (string.IsNullOrWhiteSpace(validApiKey))
+        {
+            throw new KeyNotFoundException("ApiKey not found in configuration.");
+        }
+
+        // try and get the key from a custom request header "X-ApiKey"
+        if (!context.Request.Headers.TryGetValue("X-ApiKey", out var extractedApiKey))
+        {           
+            // as a fallback option, try getting it from the query string           
+            if (!context.Request.Query.TryGetValue("X-ApiKey", out extractedApiKey))
+            { 
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("No ApiKey provided.");
+                return;
+            }
+        }         
+
+        if (!validApiKey.Equals(extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Invalid ApiKey provided.");
+            return;
+        }
+
+        await _next(context);
+    }
+}
